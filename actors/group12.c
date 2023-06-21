2e417d3f5976c688198b818d3c007ea6ae43169a
Lilaa3 <87947656+Lilaa3@users.noreply.github.com>
2023-06-21 22:21:19 +0100

    Bowser fixed, bowser specific checked removed from geo_update_layer_transparency

--- actors/group12.c
+++ actors/group12.c
@@ -12,11 +12,10 @@
 
 #include "yellow_sphere/model.inc.c"
 
+#include "impact_smoke/model.inc.c"
+
+#include "bomb/model.inc.c"
 #include "bowser/model.inc.c"
 #include "bowser/anims/data.inc.c"
 #include "bowser/anims/table.inc.c"
 #include "bowser/flames_data.inc.c"
-
-#include "impact_smoke/model.inc.c"
-
-#include "bomb/model.inc.c"
