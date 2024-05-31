1e1a045670aa32b2a1a789e4a36f72b2e85371ed
Lila <87947656+Lilaa3@users.noreply.github.com>
2024-05-31 18:44:02 +0100

    352 export

--- levels/bob/area_1/geo.inc.c
+++ levels/bob/area_1/geo.inc.c
@@ -3,7 +3,7 @@
 const GeoLayout bob_area_1_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_TRANSLATE_ROTATE_WITH_DL(LAYER_OPAQUE, 0, 0, -645, 0, 0, 180, bob_dl_GigaSuzanne_mesh_layer_1),
+		GEO_TRANSLATE_NODE_WITH_DL(LAYER_OPAQUE, 0, 0, -645, bob_dl_GigaSuzanne_mesh_layer_1),
 		GEO_DISPLAY_LIST(LAYER_OPAQUE, bob_dl_Level_Mesh_mesh_layer_1),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
